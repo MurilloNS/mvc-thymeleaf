8f10821fa95d30b3840061ab8927af6003774853
MurilloNS <murillo.murillo2001@gmail.com>
2022-04-14 08:46:14 -0300

    DepartamentoService

--- src/main/java/com/murillo/mvc/service/DepartamentoService.java
+++ src/main/java/com/murillo/mvc/service/DepartamentoService.java
@@ -1,4 +1,18 @@
 package com.murillo.mvc.service;
 
+import com.murillo.mvc.domain.Departamento;
+
+import java.util.List;
+
 public interface DepartamentoService {
+
+    void salvar(Departamento departamento);
+
+    void editar(Departamento departamento);
+
+    void excluir(Long id);
+
+    Departamento buscarPorId(Long id);
+
+    List<Departamento> buscarTodos();
 }
