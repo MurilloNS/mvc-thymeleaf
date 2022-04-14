003e6b8317ed3f1b673bbe19cd901f7bbbd0920f
MurilloNS <murillo.murillo2001@gmail.com>
2022-04-14 08:53:49 -0300

    FuncionarioService

--- src/main/java/com/murillo/mvc/service/FuncionarioService.java
+++ src/main/java/com/murillo/mvc/service/FuncionarioService.java
@@ -1,4 +1,18 @@
 package com.murillo.mvc.service;
 
+import com.murillo.mvc.domain.Funcionario;
+
+import java.util.List;
+
 public interface FuncionarioService {
+
+    void salvar(Funcionario funcionario);
+
+    void editar(Funcionario funcionario);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
 }
