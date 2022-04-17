44a6e9231c5a7cbb82ac38f27b24e90adec55623
MurilloNS <murillo.murillo2001@gmail.com>
2022-04-16 23:04:22 -0300

    Updating departamentos

--- src/main/java/com/murillo/mvc/web/controller/DepartamentoController.java
+++ src/main/java/com/murillo/mvc/web/controller/DepartamentoController.java
@@ -4,6 +4,7 @@ import com.murillo.mvc.domain.Departamento;
 import com.murillo.mvc.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,9 @@ public class DepartamentoController {
     }
 
     @GetMapping("/listar")
-    public String listar(){
+    // ModelMap - Envia a lista de Departamentos para a página
+    public String listar(ModelMap model){
+        model.addAttribute("departamentos", service.buscarTodos());
         return "/departamento/lista";
     }
 
